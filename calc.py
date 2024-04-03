6927b1d364c4a78cfd430c0f62f8ad883bcb0e17
chyeokp <165915190+chyeokp@users.noreply.github.com>
2024-04-03 22:45:32 +0900

    completed calculator

--- calc.py
+++ calc.py
@@ -1,9 +1,21 @@
-def main(): 
-
-
-def add():
-
-
-
-def divide():
+def main():
+    print("Let's implement division. Type two numbers for x and y")
     
+    x = int(input("x > "))
+    y = int(input("y > "))
+    if y == 0:
+        return print("Error: cannot divide by zero. ")
+    else:
+        return print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+def add(a, b):
+    return a + b
+    
+def divide(x, y):
+    if y == 0:
+        return print("Error: cannot divide by zero. ")
+    else:    
+        return x/y
+
+if __name__ == "__main__":
+    main()
